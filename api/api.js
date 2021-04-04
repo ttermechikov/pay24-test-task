e327caa24b551e0b6cf8e495bcab8eaa2b8e6590
Temirlan <temirlan.termechikov@gmail.com>
2021-04-05 04:14:13 +0600

    Configured cors settings.

--- api/api.js
+++ api/api.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ exposedHeaders: '*' }));
 app.use('/api/admin', routes.adminRoutes);
 app.use('/api/notes', routes.notesRoutes);
 
